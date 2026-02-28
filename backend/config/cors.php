5da0d2ba7933079c984cc9943c1edb539374408d
vladimer <vladimer177@yahoo.com>
2026-02-28 19:32:46 +0800

    cors adjustment for flutter

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -5,7 +5,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+   'allowed_origins' => [
+    'http://localhost:5173',   // Your React Web port
+    'http://192.168.0.103:8000', // Your local IP
+    '*', // Or just use this wildcard during development to stop all errors
+],
 
     'allowed_origins_patterns' => [],
 
